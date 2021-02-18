82c6d5fb8b2bf1d88ece3659bad07e3464e1a16a
socratone <gim2origin@gmail.com>
2021-02-18 18:59:29 +0900

    VueJS 카테고리 추가

--- components/Category.vue
+++ components/Category.vue
@@ -3,6 +3,11 @@
     <nav>
       <ul>
         <li class="category__link-container">
+          <router-link class="category__link" to="/category/vuejs">
+            VueJS
+          </router-link>
+        </li>
+        <li class="category__link-container">
           <router-link class="category__link" to="/category/typescript">
             TypeScript
           </router-link>
